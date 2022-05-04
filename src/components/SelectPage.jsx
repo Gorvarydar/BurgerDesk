1cd42ca34b70550ae9cba0fb046834c704f2a25c
Ilya Barysau <Ilya_Barysau@epam.com>
2022-05-04 13:37:00 +0300

    init commit

---
+++ src/components/SelectPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const SelectPage = () => {
+  return (
+    <div>
+        <select>
+            <option>dccece</option>
+            <option>dccece</option>
+            <option>dccece</option><div>cdccdc</div>
+        </select>
+    </div>
+  )
+}
